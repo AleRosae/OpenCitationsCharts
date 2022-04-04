fb2edca6b0e8fb788e29a35ab674eb0806e340c3
AleRosae <72870813+AleRosae@users.noreply.github.com>
2022-04-04 17:47:54 +0200

    app react

---
+++ react-app/src/Home.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class CreateHome extends Component {
+    render() {
+    return (
+        <div>
+            <header>
+                <h1>OpenCitations in Charts</h1>
+                <nav>
+                    <li>Home</li>
+                    <li>About</li>
+                </nav>
+            </header>
+            <div>
+            <h2>Explore the COCI dataset of OpenCitations</h2>
+            <p>Blah Blah something about OpenCitations and COCI</p>
+            </div>
+        </div>
+    )};
+}
+
+export default CreateHome;
